af31026092db19f7de3079922321c1b1a1393dde
Syny <saadraouf.dev@gmail.com>
2026-02-27 15:37:16 -0500

    telescope better seach: works on hidden ignore .git folders

--- lua/syny/plugins/telescope.lua
+++ lua/syny/plugins/telescope.lua
@@ -13,6 +13,9 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
+				file_ignore_patterns = {
+					"^.git/", -- Ignore .git folders
+				},
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -30,10 +33,27 @@ return {
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		-- Regular find files with hidden files enabled
+		keymap.set("n", "<leader>ff", function()
+			require("telescope.builtin").find_files({
+				hidden = true, -- Show hidden files
+			})
+		end, { desc = "Fuzzy find files in cwd (including hidden)" })
+		-- Find files that bypass .gitignore (shows everything)
+		keymap.set("n", "<leader>fF", function()
+			require("telescope.builtin").find_files({
+				hidden = true,
+				no_ignore = true, -- Ignore .gitignore
+				no_ignore_parent = true, -- Ignore parent .gitignore
+			})
+		end, { desc = "Fuzzy find ALL files (bypass .gitignore)" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fS", function()
+			require("telescope.builtin").live_grep({
+				additional_args = { "--no-ignore" }, -- Bypass .gitignore
+			})
+		end, { desc = "Find string in cwd (including ignored files)" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set("n", "<leader>fu", "<cmd>Telescope lsp_references<cr>", { desc = "Find Usage" })
--- lua/syny/plugins/treesitter.lua
+++ lua/syny/plugins/treesitter.lua
@@ -1,50 +1,50 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local ok, treesitter = pcall(require, "nvim-treesitter.configs")
-        if not ok then
-   return 
-  end
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+		if not ok then
+			return
+		end
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+			},
+		})
+	end,
 }
