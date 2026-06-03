305e6511633f4d968adb0dac01c2d2439ad9e087
Syny <saadraouf.dev@gmail.com>
2026-06-03 15:52:28 -0400

    better md viewer

--- lua/syny/plugins/markview.lua
+++ lua/syny/plugins/markview.lua
@@ -1,30 +1,15 @@
 return {
-{
-	"OXY2DEV/markview.nvim",
-	enabled = true,
-	lazy = false,
-	ft = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
-	opts = {
-		filetypes = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
-		buf_ignore = {},
-		max_length = 99999,
-		markdown = {
-			headings = {
-				enable = true,
-				heading_1 = { style = "simple" },
-				heading_2 = { style = "simple" },
-				heading_3 = { style = "simple" },
-				heading_4 = { style = "simple" },
-				heading_5 = { style = "simple" },
-				heading_6 = { style = "simple" },
-			},
-			tables = {
-				enable = true,
-				strict = false,
-				block_decorator = true,
-				use_virt_lines = true,
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		ft = { "markdown", "Avante" },
+		opts = {
+			file_types = { "markdown", "Avante" },
+			heading = {
+				-- Use simple ASCII icons to avoid double-width Nerd Font rendering issues
+				icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
+				signs = {},
 			},
 		},
 	},
-},
 }
